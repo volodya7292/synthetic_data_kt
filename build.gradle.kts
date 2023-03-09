47117ced4a36083fa784a1f6e9b56b218ed8195e
volodya7292 <a7292969@gmail.com>
2023-03-09 21:03:53 +0200

    add uberJar build task

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,3 +21,15 @@ dependencies {
 tasks.named<Test>("test") {
     useJUnitPlatform()
 }
+
+tasks.register<Jar>("uberJar") {
+    archiveClassifier.set("uber")
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+
+    from(sourceSets.main.get().output)
+
+    dependsOn(configurations.runtimeClasspath)
+    from({
+        configurations.runtimeClasspath.get().filter { it.name.endsWith("jar") }.map { zipTree(it) }
+    })
+}
